0e84459353bcb3239c16e99924541ad867ecb6ca
Riad Safowan <riad.safowan@northsouth.edu>
2022-06-01 23:24:22 +0600

    Updates

---
+++ precache-manifest.4b47eb3c817d7e59ccd5cc7fc5930539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdfd47e159c29e8ae4fb",
+    "url": "/chirp-frontend-react/static/css/main.fa44a388.chunk.css"
+  },
+  {
+    "revision": "cdfd47e159c29e8ae4fb",
+    "url": "/chirp-frontend-react/static/js/main.cdfd47e1.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/chirp-frontend-react/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "d8945bd626a52b579974",
+    "url": "/chirp-frontend-react/static/js/2.d8945bd6.chunk.js"
+  },
+  {
+    "revision": "34c98727671c140f3bda",
+    "url": "/chirp-frontend-react/static/js/runtime~main.34c98727.js"
+  },
+  {
+    "revision": "f351bc6bb13a731ad0db189c780a2d6f",
+    "url": "/chirp-frontend-react/index.html"
+  }
+];
